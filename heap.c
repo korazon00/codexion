56bcf6ce6a3ba085df44cea6b91868c45619db4e
Faisal  Eljeddad <faeljedd@e1r11p4.1337.ma>
2026-07-08 16:48:26 +0100

    test

--- heap.c
+++ heap.c
@@ -16,7 +16,7 @@
 // left == i * 2 + 1
 // right == i * 2 + 2
 
-void bubbledown(int heap[], int size, int i)
+void bubbledown(t_waiter heap[], int size, int i)
 {
 	int	smalleast;
 	int	left;
@@ -27,9 +27,9 @@ void bubbledown(int heap[], int size, int i)
 		smalleast = i;
 		left = i * 2 + 1;
 		right = i * 2 + 2;
-		if (left < size && heap[smalleast] > heap[left])
+		if (left < size && heap[smalleast].spawn_time > heap[left].spawn_time)
 			smalleast = left;
-		if (right < size && heap[smalleast] > heap[right])
+		if (right < size && heap[smalleast].spawn_time > heap[right].spawn_time)
 			smalleast = right;
 		if (smalleast == i)
 			break;
@@ -38,29 +38,33 @@ void bubbledown(int heap[], int size, int i)
 	}
 }
 
-void	push(int heap[], int *size, int value)
+void	push(t_waiter heap[], int *size, t_waiter coder)
 {
 	int	i;
 
-	heap[*size] = value;
+	heap[*size] = coder;
 	i = *size;
 	(*size)++;
 
-	while (i > 0 && heap[(i - 1) / 2] > heap[i])
+	while (i > 0 && heap[(i - 1) / 2].spawn_time > heap[i].spawn_time)
 	{
 		swap(&heap[i], &heap[(i - 1) / 2]);
 		i = (i - 1) / 2;
 	}
 }
 
-int	pop(int heap[], int *size)
+t_waiter	pop(t_waiter heap[], int *size)
 {
-	int	i;
-	int	min;
+	int			i;
+	t_waiter	min;
 
 	i = 0;
-	if (*size == 0)
-		return (-1);
+    if (*size == 0)
+    {
+        min.coder_id = -1;
+        min.spawn_time = 0;
+        return (min);
+    }
 	min = heap[0];
 	heap[0] = heap[*size - 1];
 	(*size)--;
@@ -68,11 +72,11 @@ int	pop(int heap[], int *size)
 	return (min);
 }
 
-void	swap(int *a, int *b)
+void	swap(t_waiter *a, t_waiter *b)
 {
-	int	i;
+	t_waiter	tmp;
 
-	i = *a;
+	tmp = *a;
 	*a = *b;
-	*b = i;
+	*b = tmp;
 }
